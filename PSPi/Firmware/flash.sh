cb8c1226843edd48c6d3c63062a8c3161870c7b6
othermod <22604540+othermod@users.noreply.github.com>
2023-06-01 10:35:56 -0400

    remove I2C firmware flashing

--- PSPi/Firmware/flash.sh
+++
@@ -1,9 +0,0 @@
-sudo killall osd
-sleep 1
-./bootloader
-sleep 1
-./stay
-sleep 1
-./stay
-sleep 1
-/home/pi/multiboot/./twiboot -a 0x29 -d /dev/i2c-1 -w flash:/home/pi/multiboot/Firmware.ino.arduino_standard.hex
