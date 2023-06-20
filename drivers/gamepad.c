89ca194684ab92d9213eadd51eb0515ae2a5b65d
othermod <22604540+othermod@users.noreply.github.com>
2023-06-20 19:52:02 -0400

    implement proper volume control
    removed the hacky method, and use alsa directly

--- drivers/gamepad.c
+++ drivers/gamepad.c
@@ -1,3 +1,4 @@
+//compile w/ gcc -O3 gamepad.c -lasound -o gamepad
 #include <linux/uinput.h>
 #include <linux/i2c-dev.h>
 #include <fcntl.h>
@@ -6,14 +7,70 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <alsa/asoundlib.h>
 
 #define I2C_DEV_PATH "/dev/i2c-1"
 #define UINPUT_DEV_PATH "/dev/uinput"
+#define VOL_INCREASE 1
+#define VOL_DECREASE -1
 
 struct uinput_user_dev uidev;
 int fd_i2c, fd_uinput;
 uint16_t previous_buttons = 65535;
 uint8_t previous_axes[2] = {0, 0};
+
+snd_mixer_t *handle;
+snd_mixer_elem_t* elem;
+
+void initialize_alsa(const char* card, const char* selem_name) {
+    snd_mixer_selem_id_t *sid;
+
+    snd_mixer_open(&handle, 0);
+    snd_mixer_attach(handle, card);
+    snd_mixer_selem_register(handle, NULL, NULL);
+    snd_mixer_load(handle);
+
+    snd_mixer_selem_id_alloca(&sid);
+    snd_mixer_selem_id_set_index(sid, 0);
+    snd_mixer_selem_id_set_name(sid, selem_name);
+
+    elem = snd_mixer_find_selem(handle, sid);
+
+    if (!elem) {
+        fprintf(stderr, "Cannot find simple control '%s',%i\n", snd_mixer_selem_id_get_name(sid), snd_mixer_selem_id_get_index(sid));
+    }
+}
+
+int change_volume(int operation, long change_value) {
+    long minv, maxv, outvol;
+
+    if (!elem) {
+        fprintf(stderr, "ALSA mixer element not initialized\n");
+        return -1;
+    }
+
+    snd_mixer_selem_get_playback_volume_range(elem, &minv, &maxv);
+    snd_mixer_selem_get_playback_volume(elem, SND_MIXER_SCHN_MONO, &outvol);
+
+    if (operation == VOL_INCREASE) {
+        outvol += change_value;
+        if (outvol > maxv) {
+            outvol = maxv;
+        }
+    } else if (operation == VOL_DECREASE) {
+        outvol -= change_value;
+        if (outvol < minv) {
+            outvol = minv;
+        }
+    } else {
+        fprintf(stderr, "Invalid operation\n");
+        return -1;
+    }
+
+    snd_mixer_selem_set_playback_volume_all(elem, outvol);
+
+    return 0;
+}
 
 void initialize_gamepad() {
     int i;
@@ -70,19 +127,13 @@ void update_gamepad() {
             ev.value = ((buttons >> i) & 1) == 0 ? 1 : 0;
             write(fd_uinput, &ev, sizeof(ev));
             previous_buttons ^= (1 << i);
-            //printf("Button %d changed\n", i);
 
             if (i == 13 && ev.value == 0) {
-              //printf("Increasing Volume\n");
-                system("amixer get Headphone | grep -o \"[0-9]*%\" | head -1 | awk -F\"%\" '{ print $1 + 5 }' | xargs -I{} amixer set Headphone {}%");
-
+                change_volume(VOL_INCREASE, 200);
             }
 
-            // Check the 6th bit of the second byte for volume down
             if (i == 14 && ev.value == 0) {
-              //printf("Decreasing Volume\n");
-              system("amixer get Headphone | grep -o \"[0-9]*%\" | head -1 | awk -F\"%\" '{ print ($1 - 5 < 50 ? 50 : $1 - 5) }' | xargs -I{} amixer set Headphone {}%");
-
+                change_volume(VOL_DECREASE, 200);
             }
         }
     }
@@ -108,11 +159,14 @@ void update_gamepad() {
 int main() {
     initialize_i2c();
     initialize_gamepad();
+    initialize_alsa("default", "Headphone");
 
     while (1) {
         update_gamepad();
         usleep(16000);
     }
+
+    snd_mixer_close(handle);
 
     return 0;
 }
