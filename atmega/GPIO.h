2b79b8b4f8e41ba6267af22b7e67c44ce3a40ff4
othermod <22604540+othermod@users.noreply.github.com>
2023-07-01 10:44:44 -0400

    move temporary definition of pins to GPIO.h so the main file is less cluttered

---
+++ atmega/GPIO.h
@@ -0,0 +1,26 @@
+// Define all of the GPIO pins in a way that the macros can read/write
+#define BTN_DISPLAY B,0
+#define ONEWIRE_LCD B,1
+#define SUPERVISOR B,2
+#define PWM_LED_ORANGE B,3
+#define SHIFT_DATA_IN B,4
+#define CLOCK B,5
+#define EN_5V0 B,6
+#define LEFT_SWITCH B,7
+
+#define BTN_SD D,0
+#define AUDIO_GAIN_1 D,1
+#define AUDIO_GAIN_0 D,2
+#define SHIFT_LOAD D,3
+#define DETECT_RPI D,4
+#define BTN_HOLD D,5
+#define LED_LEFT D,6
+#define EN_AUDIO D,7
+
+// Define all of the analog pins
+#define JOY_RX_PIN 0
+#define JOY_RY_PIN 1
+#define SENSE_SYS_PIN 2
+#define SENSE_BAT_PIN 3
+#define JOY_LX_PIN 6
+#define JOY_LY_PIN 7
--- atmega/atmega.ino
+++ atmega/atmega.ino
@@ -8,40 +8,14 @@
 #include <SPI.h>
 #include "Pin_Macros.h"
 #include "LCD_Timings.h"
+#include "GPIO.h"
 
 #define I2C_ADDRESS 0x10
 
-// Define all of the GPIO pins in a way that the macros can read/write
-#define BTN_DISPLAY B,0
-#define ONEWIRE_LCD B,1
-#define SUPERVISOR B,2
-#define PWM_LED_ORANGE B,3
-#define SHIFT_DATA_IN B,4
-#define CLOCK B,5
-#define EN_5V0 B,6
-#define LEFT_SWITCH B,7
 
-#define BTN_SD D,0
-#define AUDIO_GAIN_1 D,1
-#define AUDIO_GAIN_0 D,2
-#define SHIFT_LOAD D,3
-#define DETECT_RPI D,4
-#define BTN_HOLD D,5
-#define LED_LEFT D,6
-#define EN_AUDIO D,7
-
-#define BTN_MUTE
 
 #define DEBOUNCE_CYCLES 5 // keep the button pressed for this many loops. can be 0-255. each loop is 10ms
 
-#define JOY_RX_PIN 0
-#define JOY_RY_PIN 1
-#define SENSE_SYS_PIN 2
-#define SENSE_BAT_PIN 3
-#define JOY_LX_PIN 6
-#define JOY_LY_PIN 7
-
-byte brightness = 1;
 uint16_t detectTimeout = 0;
 
 byte arduinoInputsB;
