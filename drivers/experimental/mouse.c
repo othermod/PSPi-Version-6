667b3dda66b9b1fd67f127aab956b42b3e9644d6
othermod <22604540+othermod@users.noreply.github.com>
2023-09-16 18:11:42 -0400

    add new mouse driver

--- drivers/experimental/mouse.c
+++ drivers/experimental/mouse.c
@@ -6,13 +6,12 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/ioctl.h>
-#include <linux/i2c-dev.h>
+
 #include <linux/input.h>
 #include <linux/uinput.h>
+#include <sys/mman.h>
+#include <errno.h>
 
-#define I2C_ADDRESS 0x10
-#define AXIS_DEADZONE_LOW 107
-#define AXIS_DEADZONE_HIGH 147
 #define AXIS_CENTER 127
 
 typedef struct {
@@ -20,22 +19,12 @@ typedef struct {
     uint8_t buttonB;
     uint8_t SENSE_SYS;
     uint8_t SENSE_BAT;
-    uint8_t STATUS; // 5 bits for brightness level (can use 3 bits because there are only 8 levels), 1 for mute status, 1 for power switch, 1 for hold switch
-    uint8_t axis0;
-    uint8_t axis1;
+    uint8_t STATUS;
+    uint8_t JOY_LX;
+    uint8_t JOY_LY;
     uint8_t JOY_RX;
     uint8_t JOY_RY;
-} I2CJoystickStatus;
-
-int openI2C() {
-    int file;
-    const char * filename = "/dev/i2c-1";
-    if ((file = open(filename, O_RDWR)) < 0) {
-        fprintf(stderr, "Failed to open the i2c bus.\n");
-        exit(1);
-    }
-    return file;
-}
+} ControllerData;
 
 void emit(int virtualMouse, int type, int code, int val) {
     struct input_event ie = {0};
@@ -46,13 +35,25 @@ void emit(int virtualMouse, int type, int code, int val) {
 }
 
 int main(int argc, char * argv[]) {
-    int I2CFile = openI2C(); // open I2C device
-    if (ioctl(I2CFile, I2C_SLAVE, I2C_ADDRESS) < 0) {
-        fprintf(stderr, "I2C: Failed to acquire bus access or communicate with slave 0x%x\n", I2C_ADDRESS);
-        return 0;
+    int shm_fd;
+    ControllerData *shared_data;
+    ControllerData previous_data = {0};
+
+    while ((shm_fd = shm_open("my_shm", O_RDONLY, 0666)) == -1) {
+        if (errno == ENOENT) {
+            sleep(1);
+        } else {
+            perror("shm_open");
+            return 1;
+        }
     }
 
-    I2CJoystickStatus currentReading = {0, 0, 0, 0, 0, AXIS_CENTER, AXIS_CENTER, 0, 0};
+    shared_data = mmap(0, sizeof(ControllerData), PROT_READ, MAP_SHARED, shm_fd, 0);
+    if (shared_data == MAP_FAILED) {
+        perror("mmap");
+        close(shm_fd);
+        return 1;
+    }
 
     int virtualMouse = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
     ioctl(virtualMouse, UI_SET_EVBIT, EV_KEY);
@@ -73,21 +74,35 @@ int main(int argc, char * argv[]) {
     sleep(1);
 
     while (1) {
-        if (read(I2CFile, &currentReading, sizeof(I2CJoystickStatus)) != sizeof(I2CJoystickStatus)) {
-            printf("Controller is not detected on the I2C bus.\n");
-            sleep(1);
-        } else {
-            emit(virtualMouse, EV_REL, REL_X, (currentReading.axis0 - AXIS_CENTER) / 16);
-            emit(virtualMouse, EV_REL, REL_Y, (currentReading.axis1 - AXIS_CENTER) / 16);
-            emit(virtualMouse, EV_KEY, BTN_LEFT, ((currentReading.buttonA >> 0x03) & 1));
-            emit(virtualMouse, EV_KEY, BTN_RIGHT, ((currentReading.buttonA >> 0x06) & 1));
-            emit(virtualMouse, EV_SYN, SYN_REPORT, 0);
-        }
-        usleep(15000);
+    bool shouldEmit = false;
+
+    if (abs(shared_data->JOY_LX - AXIS_CENTER) > 15 ||
+        abs(shared_data->JOY_LY - AXIS_CENTER) > 15 ||
+        previous_data.JOY_LX != shared_data->JOY_LX ||
+        previous_data.JOY_LY != shared_data->JOY_LY) {
+
+        emit(virtualMouse, EV_REL, REL_X, (shared_data->JOY_LX - AXIS_CENTER) / 16);
+        emit(virtualMouse, EV_REL, REL_Y, (shared_data->JOY_LY - AXIS_CENTER) / 16);
+        shouldEmit = true;
     }
 
-    close(I2CFile);
+    if (previous_data.buttonA != shared_data->buttonA) {
+        emit(virtualMouse, EV_KEY, BTN_LEFT, ((shared_data->buttonA >> 0x03) & 1));
+        emit(virtualMouse, EV_KEY, BTN_RIGHT, ((shared_data->buttonA >> 0x06) & 1));
+        shouldEmit = true;
+    }
+
+    if (shouldEmit) {
+        emit(virtualMouse, EV_SYN, SYN_REPORT, 0);
+        previous_data = *shared_data;
+        usleep(8000);
+    } else {
+      usleep(20000);
+    }
+}
     ioctl(virtualMouse, UI_DEV_DESTROY);
     close(virtualMouse);
+    munmap(shared_data, sizeof(ControllerData));
+    close(shm_fd);
     return 0;
 }
