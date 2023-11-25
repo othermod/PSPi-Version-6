50d7119cc9065b50e5abf3700adedc0221ae03f8
othermod <22604540+othermod@users.noreply.github.com>
2023-11-25 10:57:43 -0500

    move configs to a folder

--- boot.sh
+++
@@ -1,9 +0,0 @@
-cd /home/pi/PSPi/Driver
-# check resolution asudo tvservice -o
-
-cd /home/pi/PSPi/Driver
-# check resolution and create config file
-cat /sys/class/graphics/fb0/virtual_size | cut -d, -f1 > pspi.cfg
-
-# start driver
-sudo ./pspi-controller &
