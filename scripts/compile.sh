f922440edbd3bb0b1e7e22ced362d56904f90acd
othermod <22604540+othermod@users.noreply.github.com>
2023-11-25 11:03:00 -0500

    add os scripts

---
+++ scripts/compile.sh
@@ -0,0 +1,4 @@
+gcc -O3 gamepad.c -o gamepad -lrt
+gcc -O3 main.c -o main -lrt
+gcc -O3 mouse.c -o mouse -lrt
+make
---
+++ scripts/lakka/autostart.sh
@@ -0,0 +1,5 @@
+/storage/system/./main &
+sleep 1
+/storage/system/./gamepad &
+/storage/system/./osd &
+bash /storage/.config/resize.sh
