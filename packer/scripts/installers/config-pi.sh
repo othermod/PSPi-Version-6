020d3e03679d49495d395f585ca80ea25a710280
Cory Manson <5052021+CoryManson@users.noreply.github.com>
2024-01-10 10:21:59 +1100

    hardcode dns

--- packer/scripts/installers/config-pi.sh
+++ packer/scripts/installers/config-pi.sh
@@ -6,13 +6,17 @@
 set -x
 
 CONFIG=/boot/config.txt
-
 locale=en_US.UTF-8
 layout=us
+
+rm /etc/resolv.conf
+echo 'nameserver 8.8.8.8' > /etc/resolv.conf
+
 raspi-config nonint do_change_locale $locale
 raspi-config nonint do_configure_keyboard $layout
 raspi-config nonint do_hostname pspi6
-raspi-config nonint do_expand_rootfs
+raspi-config nonint do_expand_rootfs 
+raspi-config nonint do_change_timezone UTC
 cat <<EOF >> $CONFIG
 # Don't show splash screen
 disable_splash=1
