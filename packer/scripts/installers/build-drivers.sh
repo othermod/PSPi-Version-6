a00ba7a4538bb9b51bb475c7ca3c96078bb34923
Cory Manson <5052021+CoryManson@users.noreply.github.com>
2024-02-05 09:21:44 +1100

    add patchelf

--- packer/scripts/installers/build-drivers.sh
+++ packer/scripts/installers/build-drivers.sh
@@ -3,6 +3,7 @@
 ##  File:  build-drivers.sh
 ##  Desc:  Installs required dependencies for PSPi6 drivers & builds them
 ################################################################################
+set -x
 
 # Install Dependencies
 apt-get update
@@ -11,3 +12,15 @@ apt-get install make libraspberrypi-dev raspberrypi-kernel-headers libpng-dev li
 cd /packer/drivers
 make clean
 make all
+
+# Build patchelf
+git clone https://github.com/NixOS/patchelf.git
+cd patchelf
+./bootstrap.sh
+./configure
+make
+make check
+make install
+
+# Patch OSD Driver
+patchelf --replace-needed libbcm_host.so.0 libbcm_host.so ../osd
