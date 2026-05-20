2ed57afb008fbdc8bb63cb91584446689265d4f0
othermod <admin@othermod.com>
2026-05-19 22:22:23 -0400

    fix recalbox cm4 download link and version

--- scripts/distros/recalbox.sh
+++ scripts/distros/recalbox.sh
@@ -18,7 +18,7 @@ ALL_TARGETS=(cm4)
 
 declare -A TARGET_URL TARGET_SHA256 TARGET_PSPI_PREFIX TARGET_BIN
 
-TARGET_URL[cm4]="https://github.com/recalbox/recalbox-os/releases/download/v8.0.3/recalbox-rpi4_64.img.xz"
+TARGET_URL[cm4]="https://upgrade.recalbox.com/latest/download-wizard/rpi4_64/recalbox-rpi4_64.img.xz"
 TARGET_SHA256[cm4]=""
-TARGET_PSPI_PREFIX[cm4]="Recalbox8-PSPi6"
+TARGET_PSPI_PREFIX[cm4]="Recalbox10.0.0.5-PSPi6"
 TARGET_BIN[cm4]=64
