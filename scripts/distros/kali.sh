72930e99b4c52f248c59bed82db8400dd303946b
othermod <admin@othermod.com>
2026-08-08 18:40:18 -0400

    kali works on cm5, so add that to image name

--- scripts/distros/kali.sh
+++ scripts/distros/kali.sh
@@ -5,7 +5,7 @@ INIT_SYSTEM="systemd"
 
 # Kali ships armhf and arm64 Raspberry Pi images per release.
 # armhf zero2 image: boots on Zero 2 W, Zero 1, and CM4
-# arm64 pi image: boots on Pi 4, Pi 400, and CM4
+# arm64 pi image: boots on Pi 4, Pi 400, CM4, and CM5
 ALL_TARGETS=(zero2 cm4)
 
 declare -A TARGET_URL TARGET_SHA256 TARGET_PSPI_PREFIX TARGET_BIN
@@ -17,7 +17,7 @@ TARGET_SHA256[zero2]="c16ab9511353db77d9e324ba804525b047f7984025672a6ee636fd720e
 TARGET_SHA256[cm4]="795495b65b31e567613304cdfcccf67e0e1e5d018a006fdee00a5b5d2d49d72c"
 
 TARGET_PSPI_PREFIX[zero2]="Kali2026.2-Zero2-PSPi6"
-TARGET_PSPI_PREFIX[cm4]="Kali2026.2-CM4-PSPi6"
+TARGET_PSPI_PREFIX[cm4]="Kali2026.2-CM4-CM5-PSPi6"
 
 TARGET_BIN[zero2]=32
 TARGET_BIN[cm4]=64
